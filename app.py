d9792a87273165e9b011064550cae922d1b069c4
Barnes2197 <mauriceb2197@gmail.com>
2020-10-02 20:35:13 -0400

    Added tuition route to check the tuition of any college and added a before_request method to load data source before serving request

--- app.py
+++ app.py
@@ -15,14 +15,26 @@ from flask.signals import Namespace
 
 app = Flask(__name__)
 app.config["DEBUG"] = False
+schools = {}
+@app.before_first_request
+def load_csv():
+    with open('tuition_cost.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if '/' in row['name']:
+                row['name'].replace('/', ' ')
+            schools[row['name']] = row
+
 
 @app.route('/', methods=['GET'])
 def home():
     response = {}
     response['message'] = 'Hello'
     return jsonify(response)
+
 @app.route('/names', methods=['GET'])
 def names():
+
     # def school_has_name(school):
     #     try:
     #         name = school.get('name')
@@ -32,13 +44,27 @@ def names():
     #         return ''
     #snapshots = doc_ref.get()
     #document.get('name') to retrieve all names
+
     college_names = []
-    with open('tuition_cost.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            college_names.append(row['name'])
-    print(list(college_names))
+    for school in schools.items():
+            college_names.append(school[0])
+        
     return jsonify(college_names)
+
+@app.route('/tuition/', methods=['GET'])
+def tuition():
+    school_name = request.args['name']
+    print(request.args['name'])
+    tuition_cost = {}
+    print(schools[school_name])
+    try:
+        tuition_cost['in_state_tuition'] = schools[school_name]['in_state_tuition']
+        tuition_cost['out_of_state_tuition'] = schools[school_name]['out_of_state_tuition']
+        return jsonify(tuition_cost)
+    except:
+        return 'School name not in database'
+
+
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
